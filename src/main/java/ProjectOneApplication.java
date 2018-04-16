cacb087bb7d8d070a51e7ce8fe64b5b56f94ba50
Viet <vietsemantics@gmail.com>
2018-04-16 02:46:57 -0500

    init project

---
+++ src/main/java/ProjectOneApplication.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Viet Quoc Tran vt
+ * on 4/16/18.
+ * www.zeroexception.com
+ */
+public class ProjectOneApplication {
+
+
+    public static void main(String ... args) {
+
+        List<String> list = new LinkedList<>();
+        list.forEach(System.out::print);
+    }
+}
