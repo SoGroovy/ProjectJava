0637dca14bb6785ea2d0ffe77c3327de6e97b465
Viet <vietsemantics@gmail.com>
2018-04-16 02:47:57 -0500

    edit

--- src/main/java/ProjectOneApplication.java
+++ src/main/java/ProjectOneApplication.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * @author Viet Quoc Tran vt
  * on 4/16/18.
@@ -11,7 +8,8 @@ public class ProjectOneApplication {
 
     public static void main(String ... args) {
 
-        List<String> list = new LinkedList<>();
-        list.forEach(System.out::print);
+        System.out.println("Hello");
     }
+
+
 }
