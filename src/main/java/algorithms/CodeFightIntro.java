bee593784f7cd2c3159b4ab8a7f96b1a2baca3b0
Tin Phan <tinphan09@gmail.com>
2018-05-04 17:11:25 -0500

    check palindrome

--- src/main/java/algorithms/CodeFightIntro.java
+++ src/main/java/algorithms/CodeFightIntro.java
@@ -19,4 +19,37 @@ public interface CodeFightIntro {
         return param1 + param2;
     }
 
+    /**
+     * This function is from https://codefights.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
+     * @param year
+     * @return
+     */
+    static int centuryFromYear(int year) {
+        if (year%100==0){
+            return (year/100);
+        } else {
+            return (year/100) +1;
+        }
+    }
+
+    /**
+     * This function is from https://codefights.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ/description
+     */
+    import java.util.*;
+    public class checkPalindrome{
+        public static void main(String[] args);
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type a string to check for palindrome");
+        String s = in.nextLine();
+
+        reversedString = new Stringbuilder(s).reverse.toString();
+
+    if(s.equals(reversedString)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/test/java/algorithms/CodeFightIntroTest.java
+++ src/test/java/algorithms/CodeFightIntroTest.java
@@ -3,6 +3,8 @@ package algorithms;
 import org.junit.Test;
 
 import static algorithms.CodeFightIntro.add;
+import static algorithms.CodeFightIntro.centuryFromYear;
+import static algorithms.CodeFightIntro.istPalindrom;
 import static org.junit.Assert.*;
 
 /**
@@ -20,4 +22,35 @@ public class CodeFightIntroTest {
         assertEquals(3,a);
     }
 
+    @Test
+    public void testCenturyFromYear() {
+        int year = 2000;
+        int expectedValue = 20;
+        int actualValue = centuryFromYear(year);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void testCenturyFromYear1905() {
+        int year = 1905;
+        int century = centuryFromYear(year);
+        assertEquals(20, century);
+    }
+    @Test
+    public void testCenturyFromYear1700(){
+        int year = 1700;
+        int century = centuryFromYear(year);
+        assertEquals(17, century);
+    }
+    @Test
+    public void testCenturyFromYear1988(){
+        int year = 1988;
+        int century = centuryFromYear(year);
+        assertEquals(20, century);
+    }
+    @Test
+    public void testCheckPalindrome(){
+        String input = "aabaa";
+        String palin = istPalindrom(input);
+        assertEquals(true, palin);
+    }
 }
