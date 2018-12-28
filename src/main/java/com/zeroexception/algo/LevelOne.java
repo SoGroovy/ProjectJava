8b87dc84e19b55fdcb9e0d011b973b863a91aa82
Tin Phan <tinphan09@gmail.com>
2018-12-27 19:59:57 -0600

    downHill

--- src/main/java/com/zeroexception/algo/LevelOne.java
+++ src/main/java/com/zeroexception/algo/LevelOne.java
@@ -105,7 +105,12 @@ public interface LevelOne {
 
 
   static int getDownHillIndex(int[] arr){
-
-    return 0;
+    int downHill = 0;
+    for(int i = 0; i < arr.length-1; i++){
+      if(arr[i] > arr[i+1]) {
+        downHill = (i + 1);
+      }
+    }
+    return downHill;
   }
 }
